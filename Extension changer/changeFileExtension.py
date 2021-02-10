203a1f5e4bde2798f79939191d7d7e617025831c
Gautam Pranjal <gautam17pranjal@gmail.com>
2021-02-10 14:23:18 -0800

    file ext change to .srt

--- Extension changer/changeFileExtension.py
+++ Extension changer/changeFileExtension.py
@@ -1,12 +1,13 @@
+# changing file extension from .vtt to .srt
+
 import os, sys
 
 folder = "Extension changer/test/"
 files = os.listdir(folder)
 for file in files:
-    infilename = os.path.join(folder, file)
-    if not os.path.isfile(infilename):
+    oldname = os.path.join(folder, file)
+    if not os.path.isfile(oldname):
         continue
-    oldbase = os.path.splitext(file)
-    newname = infilename.replace('.vtt', '.srt')
-    output = os.rename(infilename, newname)
+    newname = oldname.replace('.vtt', '.srt')
+    output = os.rename(oldname, newname)
 print("done")
